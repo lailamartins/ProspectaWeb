12edc4899e08eebebffc452901fd5f95e3e969fb
lailamartins <laila.costa.1471720@sga.pucminas.br>
2023-10-20 16:38:11 -0300

    Adicionar arquivos de projeto.

---
+++ ProspectaWeb/Models/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProspectaWeb.Models
+{
+
+    [Table("Clientes")]
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Nascimento { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Empresa { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public int Produto { get; set; }
+
+    }
+}
